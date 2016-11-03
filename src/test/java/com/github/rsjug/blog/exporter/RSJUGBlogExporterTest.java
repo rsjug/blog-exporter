86bbec45babc88f3c5493408315f1382d0ccf332
rmpestano <bigboss666>
2016-11-03 00:07:06 -0200

    perfumery

--- src/test/java/com/github/rsjug/blog/exporter/RSJUGBlogExporterTest.java
+++ src/test/java/com/github/rsjug/blog/exporter/RSJUGBlogExporterTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -17,6 +18,7 @@ import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.contentOf;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by pestano on 02/11/16.
@@ -47,6 +49,19 @@ public class RSJUGBlogExporterTest {
                 "lead_text: 'O RSJUG é o grupo de usuários da linguagem Java do Rio Grande do Sul - Brasil. O JUG começou na UFRGS (Universidade Federal do Rio Grande do Sul) , entre os alunos do Centro de Pós-Graduação em Ciências da Computaçao. O principal idealizador e primeiro JU...'" + NEW_LINE +
                 "---");
         assertThat(contentOf(generatedPost)).contains("<p style=\"text-align: justify;\">O RSJUG é o grupo de usuários da linguagem Java do Rio Grande do Sul - Brasil.");
+
+    }
+
+    @Test
+    public void shouldExportBlogViaCommandLine() throws IOException {
+        new RSJUGBlogExporter().execute(new String[]{
+                "-outputDir", "\""  + PATH+"/command-line/" +"\"",
+                "-p", "\""  + RSJUGBlogParser.class.getResource("/sample-feed.xml").getFile() +"\""
+        });
+        File generatedPost = new File(PATH +"/command-line/2008-10-11-Histórico.md");
+        assertThat(generatedPost).exists();
+        File images = new File(PATH +"/img");
+        assertThat(images).exists().isDirectory();
 
     }
 
